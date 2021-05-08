bb4f9795267c26c20840ac6faf6c06b92c2ddc70
rikaami <rika.amijayanti@gmail.com>
2021-05-08 18:27:52 +0700

    BoilerPlate and install react-bootstrap

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import BootsNav from './components/BootsNav'
+
+function App() {
+  return (
+    <div className="App">
+    <h1> Hello React</h1>
+    <BootsNav />
+    </div>
+  );
+}
+
+export default App;
